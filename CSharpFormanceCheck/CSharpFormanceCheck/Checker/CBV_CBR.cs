fe2fee994f37175bf799425954b522f861d2357b
encyless <encyless@gmail.com>
2021-05-02 12:47:07 +0900

    CBV vs CBR - Class
    사실 CBV vs CBR 을 해보게 되면, 누군가는 CBR이 무조건 좋다고 생각 할 수 있다.
    그러나 class는 애초에 인자로 전달시에는 포인터로 연산을 진행하게 된다.

---
+++ CSharpFormanceCheck/CSharpFormanceCheck/Checker/CBV_CBR.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace CSharpFormanceCheck.Checker
+{
+    [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+    public class CheckerCBVCBR
+    {
+        public class Specimen
+        {
+            public int ID;
+            public int Atk;
+            public int Hp;
+        }
+
+        private const int N = 10000;
+        private Specimen data = new Specimen();
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public CheckerCBVCBR()
+        {
+        }
+
+        [Benchmark]
+        public int CallByValue()
+        {
+            return CBV(data);
+        }
+
+        private int CBV(Specimen spmen)
+        {
+            spmen.Atk++;
+            return spmen.Atk;
+        }
+
+        [Benchmark]
+        public int CallByRef()
+        {
+            return CBR(ref data);
+        }
+
+        private int CBR(ref Specimen spmen)
+        {
+            spmen.Atk++;
+            return spmen.Atk;
+        }
+    }
+}
---
+++ CSharpFormanceCheck/CSharpFormanceCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CSharpFormanceCheck.Checker;
+
+namespace MyBenchmarks
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //check CBV vs CBR 
+            var summary = BenchmarkRunner.Run<CheckerCBVCBR>();
+        }
+    }
+}
