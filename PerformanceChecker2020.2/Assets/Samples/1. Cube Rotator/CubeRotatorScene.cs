0b3b260469b81661cf96d062968e46c0282a6aa9
encyless <encyless@gmail.com>
2021-05-13 13:14:53 +0900

    Unity git ignore added

---
+++ PerformanceChecker2020.2/Assets/Samples/1. Cube Rotator/CubeRotatorScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotatorScene : MonoBehaviour
+{
+    public GameObject prefab;
+    public int NumOfObject = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < NumOfObject; ++i)
+        {
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
